681254c7ab4812a99ca1c3d4105cf7ea463198d8
heb3r23 <m.h3b3r28@gmail.com>
2024-09-12 17:00:12 -0700

    Completed Lab #4

--- hmorenofuentes4.c
+++ hmorenofuentes4.c
@@ -3,22 +3,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
+int x;
+int y = 15;
 
+int main(int argc, char *argv[]){
+	int i;
+	int z = 0;
 	int* values;
 	values = (int *)malloc(sizeof(int)*5);
-	int x;
-        int y = 15;
+
+
 
 	int* argcPointer = &argc;
-	int* stackPointer = values;
-	int** heapPointer = &values;
+	int* stackPointer = &i;
+	int* heapPointer = values;
 	int* uninPointer = &x;
 	int* inPointer = &y;
 
+	printf("Stack variable address: %p\n", stackPointer);
 	printf("argc address: %p\n", argcPointer);
-	printf("Stack address: %p\n", stackPointer);
-	printf("Heap address: %p\n", heapPointer);
+	printf("Heap variable address: %p\n", heapPointer);
 	printf("Uninitialized variable address: %p\n", uninPointer);
 	printf("Initialized variable address: %p\n", inPointer);
 
